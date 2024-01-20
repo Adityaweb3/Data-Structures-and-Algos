f374c94e0f7af66630c78aebcdb4bc7195d8f7e9
adityaweb3 <ap7596112@gmail.com>
2024-01-20 20:05:01 +0530

    pair with given difference

---
+++ DSA 450/searching  & sorting/pairs_with_a_given_difference.cpp
@@ -0,0 +1,17 @@
+// Given an array Arr[] of size L and a number N, 
+// you need to write a program to find if there exists a pair of elements in the array whose difference is N.
+
+ sort(arr, arr+size) ; 
+    
+     int i = 0, j = 1;
+        while (i < size && j < size) {
+            if (arr[j] - arr[i] == n && j != i) {
+                return true;
+            }
+            if (arr[j] - arr[i] > n) {
+                i++;
+            } else {
+                j++;
+            }
+        }
+        return false;
