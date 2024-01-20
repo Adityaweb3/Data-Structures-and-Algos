a5c72e240d07366ff83099ed5fe42ebe1d0ce7e1
adityaweb3 <ap7596112@gmail.com>
2024-01-20 16:57:47 +0530

    first and last occurance'

--- DSA 450/searching  & sorting/first_and_last_index.cpp
+++ DSA 450/searching  & sorting/first_and_last_index.cpp
@@ -2,76 +2,78 @@
 // the task is to find the first and last occurrences of an element x in the given array.
 // Note: If the number x is not found in the array then return both the indices as -1.
 
-int firstOccurance(int arr[] , int n , int x){
-        
-        int low = 0 ; int high = n-1 ;
-        while(low<=high){
-            int mid = low + (high-low)/2 ;
-            
-            if(arr[mid]>x){
-                high = mid-1 ;
-                
-            }
-            
-            if(arr[mid]<x){
-                low = mid+1 ;
-            }
-            
-            if(arr[mid]==x) {
-                if(mid== 0 || arr[mid-1]!=x){
-                    return mid ;
-                }
-                
-                else {
-                    high = mid -1 ;
-                }
-            }
-            
-            
-            
-        }
-        
-        return -1 ; 
-        
-        
-    }
+ int getFirst(int arr[], int n, int x){
+	int low = 0 ;
+	int high = n-1 ;
 
-     int lastOccurance(int arr[] , int n , int x){
-        
-        int low = 0 ; int high = n-1 ; 
-        
-        while(low<=high){
-            int mid = low + (high-low)/2 ;
-            
-            if(arr[mid]>x){
-                high = mid-1 ;
-                
-            }
-            
-            if(arr[mid]<x){
-                low = mid+1 ;
-            }
-            
-            if(arr[mid]==x) {
-                if(mid== 0 || arr[mid+1]!=x){
-                    return mid ;
-                }
-                
-                else {
-                    low = mid+1 ;
-                }
-            }
-            
-        }
-        
-        return -1 ;
-    }
+	while(low<=high){
+		int mid = low +(high-low)/2 ;
+
+		if(arr[mid] >x){
+		   high = mid-1 ;
+
+		}
+
+		if(arr[mid] <x){
+			low = mid+1 ;
+		}
+
+		if(arr[mid]==x){
+			if(mid== 0 || arr[mid-1]!=arr[mid]){
+				return mid ;
+			}
+
+			else{
+				high = mid-1 ;
+			}
+		}
+	}
+
+	return -1 ;
+}
+
+
+
+int getLast(int arr[], int n, int x){
+	int low = 0 ;
+	int high = n-1 ;
+
+	while(low<=high){
+		int mid = low +(high-low)/2 ;
+
+		if(arr[mid] >x){
+		   high = mid-1 ;
+
+		}
+
+		if(arr[mid] <x){
+			low = mid+1 ;
+		}
+
+		if(arr[mid]==x){
+			if(mid== n-1 || arr[mid+1]!=arr[mid]){
+				return mid ;
+			}
+
+			else{
+				low= mid+1 ;
+			}
+		}
+	}
+
+	return -1 ;
+}
+
+    
+   
+
+
     vector<int> find(int arr[], int n , int x )
     {
         // code here
         
-        int first = firstOccurance(arr , n , x) ;
-        int last = lastOccurance(arr, n , x) ; 
+        int first = getFirst(arr , n , x) ;
+        int last = getLast(arr, n , x) ; 
         
         return {first,last} ;
     }
