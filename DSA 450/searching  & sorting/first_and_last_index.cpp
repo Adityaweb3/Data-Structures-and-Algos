a35269b5fa01372f11e4699cfcce731d7df2af00
adityaweb3 <ap7596112@gmail.com>
2024-01-20 16:23:35 +0530

    first and last occurance

---
+++ DSA 450/searching  & sorting/first_and_last_index.cpp
@@ -0,0 +1,41 @@
+// Given a sorted array arr containing n elements with possibly some duplicate, 
+// the task is to find the first and last occurrences of an element x in the given array.
+// Note: If the number x is not found in the array then return both the indices as -1.
+
+int firstOccurance(int arr[] , int n , int x){
+        
+        int low = 0 ; int high = n-1 ;
+        while(low<=high){
+            int mid = low + (high-low)/2 ;
+            
+            if(arr[mid]>x){
+                high = mid-1 ;
+                
+            }
+            
+            if(arr[mid]<x){
+                low = mid+1 ;
+            }
+            
+            if(arr[mid]==x) {
+                if(mid== 0 || arr[mid-1]!=x){
+                    return mid ;
+                }
+                
+                else {
+                    high = mid -1 ;
+                }
+            }
+            
+            
+            
+        }
+        
+        return -1 ; 
+        
+        
+        
+    
+        
+        
+    }
