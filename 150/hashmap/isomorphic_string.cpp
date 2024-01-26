d03008a6e39044344ee7b1306b4e2efe6e3609ea
adityaweb3 <ap7596112@gmail.com>
2024-01-26 17:50:10 +0530

    isomorphic strings

---
+++ 150/hashmap/isomorphic_string.cpp
@@ -0,0 +1,27 @@
+// Given two strings s and t, determine if they are isomorphic.
+// Two strings s and t are isomorphic if the characters in s can be replaced to get t.
+// All occurrences of a character must be replaced with another character while preserving the order of characters. 
+// No two characters may map to the same character, but a character may map to itself.
+
+bool areIsomorphic(string &str1, string &str2)
+{
+    // Write your code here
+    unordered_map<char , char>mp , mp2 ; 
+
+    for(int i = 0 ; i<str1.length() ; i++){
+        if(mp[str1[i]] && mp[str1[i]] != str2[i]){
+            return false ;
+        }
+
+        if(mp2[str2[i]] && mp2[str2[i]] != str1[i]){
+            return false ;
+        }
+
+        mp[str1[i]] = str2[i] ; 
+        mp2[str2[i]] = str1[i] ;
+    }
+
+    return true ;
+
+    
+}
