1d3d5d9a3112bf56b4ce85d2e6c01de1f3b979fc
adityaweb3 <ap7596112@gmail.com>
2024-02-23 20:57:21 +0530

    pairwise swap of node

---
+++ pairwise_swap_nodes.cpp
@@ -0,0 +1,23 @@
+// Problem Link : https://leetcode.com/problems/swap-nodes-in-pairs/description/
+
+ListNode* swapPairs(ListNode* head) {
+        if(head==NULL || head->next==NULL){
+            return head ;
+        }
+
+        ListNode *curr = head->next->next ; 
+        ListNode * prev = head ; 
+        head = head->next ; 
+        head->next= prev ;
+
+        while(curr!=NULL && curr->next!=NULL){
+            prev->next = curr->next ; 
+            prev = curr ; 
+            ListNode *next = curr->next->next ; 
+            curr->next->next = curr ; 
+            curr = next ;
+        }
+
+        prev->next = curr ; 
+        return head ;
+    }
