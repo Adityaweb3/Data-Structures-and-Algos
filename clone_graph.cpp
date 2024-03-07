a4666ee117518817995fb6118e5713d4a4d66d41
adityaweb3 <ap7596112@gmail.com>
2024-03-07 19:14:58 +0530

    clone graph

---
+++ clone_graph.cpp
@@ -0,0 +1,27 @@
+// Problem Link : https://leetcode.com/problems/clone-graph/description/
+
+ void dfs(Node *node , Node *copy , vector<Node *>&vis){
+        vis[copy->val]= copy ; 
+        for(auto x : node->neighbors){
+            if(vis[x->val]==NULL){
+                Node *newNode = new Node(x->val) ; 
+                (copy->neighbors).push_back(newNode) ; 
+                dfs(x , newNode , vis) ; 
+            }
+
+            else {
+                (copy->neighbors).push_back(vis[x->val]);
+            }
+
+        }
+    }
+    Node* cloneGraph(Node* node) {
+         if(node==NULL){
+             return node ; 
+         }
+
+         vector<Node *>vis(1000 , NULL) ; 
+         Node *copy = new Node(node->val) ; 
+         dfs(node , copy , vis) ; 
+         return copy ; 
+    }
