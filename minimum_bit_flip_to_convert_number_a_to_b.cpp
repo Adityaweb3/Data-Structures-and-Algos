bbb53b7995ebe56fe8e508661e2de860937d6cd0
adityaweb3 <ap7596112@gmail.com>
2024-03-09 23:09:30 +0530

    minimum number of flips to convert a to b

---
+++ minimum_bit_flip_to_convert_number_a_to_b.cpp
@@ -0,0 +1,6 @@
+// Problem Link : https://leetcode.com/problems/minimum-bit-flips-to-convert-number/description/
+
+int minBitFlips(int start, int goal) {
+        int ans = start^goal ; 
+        return __builtin_popcount(ans);
+    }
