536dea66ee18f5d06d3f2d9c7c075b11f3302e2d
adityaweb3 <ap7596112@gmail.com>
2024-03-19 09:32:21 +0530

    tasks scheduler

---
+++ task_scheduler.cpp
@@ -0,0 +1,15 @@
+// Problem Link : https://leetcode.com/problems/task-scheduler/description/
+
+ int leastInterval(vector<char>& tasks, int n) {
+        vector<int>freq(26,0) ; 
+        for(int i = 0 ; i<tasks.size() ; i++){
+            freq[tasks[i]-'A']++ ; 
+        }
+        sort(freq.begin() , freq.end() ,greater<int>()) ;
+        int idle = (freq[0]-1)*n ; 
+        for(int i =1 ; i<26 ; i++){
+            idle-=min(freq[i] , freq[0]-1) ;
+        }
+
+        return tasks.size()+max(0,idle) ;
+    }
