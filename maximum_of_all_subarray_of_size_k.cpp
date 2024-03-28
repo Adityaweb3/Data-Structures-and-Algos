19e883ce226b044a48a3cdb637196b536b297c31
adityaweb3 <ap7596112@gmail.com>
2024-03-28 09:24:01 +0530

    maximum of all subarray of size k

---
+++ maximum_of_all_subarray_of_size_k.cpp
@@ -0,0 +1,26 @@
+// Problem Link : https://www.naukri.com/code360/problems/maximum-of-all-subarrays-of-size-k_1170789
+
+class Solution {
+public:
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        deque<int>dq ; 
+        vector<int>ans ; 
+
+        for(int i = 0 ; i<nums.size() ; i++){
+            if(!dq.empty() && dq.front()==i-k){
+                dq.pop_front() ;
+            }
+
+            while(!dq.empty() && nums[dq.back()]<=nums[i]){
+                dq.pop_back() ;
+            }
+
+            dq.push_back(i) ;
+            if(i>=k-1){
+                ans.push_back(nums[dq.front()]) ;
+            }
+        }
+
+        return ans ;
+    }
+};
