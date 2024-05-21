3075966f25d11975a2b02879c87cc78a6c85a1ae
adityaweb3 <ap7596112@gmail.com>
2024-05-21 18:38:57 +0530

    group anagrams

---
+++ neetcode_150/array_hashing/anagrams_group.cpp
@@ -0,0 +1,23 @@
+// Problem Link : https://neetcode.io/problems/anagram-groups
+
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        //create a map with key as the string and value as a vector 
+        unordered_map<string , vector<string>>mp ; 
+        vector<vector<string>>ans ; 
+
+        for(int i = 0 ; i<strs.size() ; i++){
+            string temp = strs[i] ; 
+            sort(temp.begin() , temp.end()) ; 
+
+            mp[temp].push_back(strs[i]) ; 
+        }
+
+        for(auto i : mp){
+            ans.push_back(i.second) ; 
+        }
+
+        return ans ; 
+    }
+};
