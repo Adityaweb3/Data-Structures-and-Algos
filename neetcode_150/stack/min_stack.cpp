129d10bcc53af2dc72c7e1c73e971e1947db62ec
adityaweb3 <ap7596112@gmail.com>
2024-05-21 23:18:59 +0530

    min stack

---
+++ neetcode_150/stack/min_stack.cpp
@@ -0,0 +1,36 @@
+// Problem Link : https://leetcode.com/problems/min-stack/description/
+
+class MinStack {
+public:
+  vector<pair<int , int>>st ;
+    MinStack() {
+        
+    }
+    
+    void push(int val) {
+        if(st.empty()){
+            pair<int , int>p ; 
+            p.first = val ; 
+            p.second = val ; 
+            st.push_back(p) ; 
+        }
+        else {
+            pair<int , int>p ; 
+            p.first = val ; 
+            p.second = min(val , st.back().second) ; 
+            st.push_back(p) ; 
+        }
+    }
+    
+    void pop() {
+        st.pop_back() ; 
+    }
+    
+    int top() {
+        return st.back().first ; 
+    }
+    
+    int getMin() {
+        return st.back().second ; 
+    }
+};
