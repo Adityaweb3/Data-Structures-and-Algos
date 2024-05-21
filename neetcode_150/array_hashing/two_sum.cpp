f075be7fbdbe321f49a8f9795d529fdeac0a480f
adityaweb3 <ap7596112@gmail.com>
2024-05-21 18:25:24 +0530

    two sum

---
+++ neetcode_150/array_hashing/two_sum.cpp
@@ -0,0 +1,20 @@
+// Problem Link : https://neetcode.io/problems/two-integer-sum
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n = nums.size() ; 
+        unordered_map<int , int>mp ;
+
+        for(int i = 0 ; i<n ; i++){
+            if(mp.find(target-nums[i])!=mp.end()){
+                return {mp[target-nums[i]] , i} ; 
+            }
+            else {
+                mp[nums[i]]=i ; 
+            }
+        }
+
+        return {-1 , -1} ; 
+    }
+};
