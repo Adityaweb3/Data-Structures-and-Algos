947ea35e3e6a68b13da31743a53a76a3e76b623a
adityaweb3 <ap7596112@gmail.com>
2024-05-27 21:14:04 +0530

    sort array by parity

---
+++ sort_array_by_parity.cpp
@@ -0,0 +1,24 @@
+// Problem link : https://leetcode.com/problems/sort-array-by-parity/description/
+
+class Solution {
+public:
+    vector<int> sortArrayByParity(vector<int>& nums) {
+       int n = nums.size() ; 
+       int i =0 ; 
+       int j =0 ; 
+       while(j<n){
+        if(nums[j]%2==0){
+            swap(nums[i] , nums[j]) ; 
+            i++ ; 
+            j++ ; 
+        }
+
+        else {
+            j++ ; 
+        }
+       }
+
+       return nums ; 
+
+    }
+};
