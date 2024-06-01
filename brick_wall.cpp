b6f8b29fc99cdb616facdd7420e1b391fea94c09
adityaweb3 <ap7596112@gmail.com>
2024-06-01 15:56:01 +0530

    brick walls

---
+++ brick_wall.cpp
@@ -0,0 +1,20 @@
+// Problem Link : https://leetcode.com/problems/brick-wall/description/
+
+class Solution {
+public:
+    int leastBricks(vector<vector<int>>& wall) {
+        unordered_map<int ,int>mp ; 
+        int n = wall.size() ; 
+        int ans =0 ; 
+        for(int i = 0 ; i<n ; i++){
+            int sum = 0 ; 
+            for(int j =0 ; j<wall[i].size()-1; j++){
+                sum+=wall[i][j] ; 
+                mp[sum]++ ; 
+                ans = max(ans , mp[sum]);
+            }
+        }
+
+        return n-ans ; 
+    }
+};
