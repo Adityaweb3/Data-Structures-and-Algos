073f624e7a4cfd409f32094778922b92c8f52d92
adityaweb3 <ap7596112@gmail.com>
2024-06-16 13:01:38 +0530

    shortest path in undirected weighted graph

--- graph 56/shortest_path_in_undirected_weighted_graph.cpp
+++ graph 56/shortest_path_in_undirected_weighted_graph.cpp
@@ -1,1 +1,51 @@
-Problem Link : 
+// Problem Link : https://www.geeksforgeeks.org/problems/shortest-path-in-weighted-undirected-graph/
+
+ vector<int> shortestPath(int n, int m, vector<vector<int>>& edges) {
+        // Code here
+       vector<vector<pair<int,int>>> adj(n+1);
+        
+        for(auto &it:edges){
+            adj[it[0]].push_back({it[1],it[2]});
+            adj[it[1]].push_back({it[0],it[2]});
+        }
+        
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
+        vector<int> dist(n+1,1e9), parent(n+1);
+        for(int i=1;i<=n;i++) parent[i] = i;
+        
+        dist[1] = 0;
+        pq.push({0,1});
+        while(!pq.empty()){
+            auto it = pq.top();
+            int dis = it.first;
+            int node = it.second;
+            pq.pop();
+            
+   
+            
+            for(auto &it:adj[node]){
+                int adjNode = it.first;
+                int edW = it.second;
+                
+                if(dis + edW < dist[adjNode]){
+                    dist[adjNode] = dis + edW;
+                    pq.push({dist[adjNode],adjNode});
+                    parent[adjNode] = node;
+                }
+            }
+        }
+        
+        // if destination is not reachable 
+        if(dist[n]==1e9) return {-1};
+        
+        vector<int> path;
+        int node = n;
+        while(parent[node] != node){
+            path.push_back(node);
+            node = parent[node];
+        }
+        path.push_back(1);
+        path.push_back(dist[n]);
+        reverse(path.begin(),path.end());
+        return path;
+    }
